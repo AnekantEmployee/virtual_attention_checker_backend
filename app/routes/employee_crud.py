187d0fd5dacc9a4dde75f9405576f652bcd5cdf0
Anekant  Jain <anekant.jain@Yash.com>
2025-03-27 20:22:17 +0530

    Some indetations

--- app/routes/employee_crud.py
+++ app/routes/employee_crud.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 employee_crud = Blueprint("employee_crud", __name__)
 
+
 @employee_crud.route("/create-employee", methods=["POST"])
 def create_employee():
     data = request.get_json()
@@ -22,7 +23,9 @@ def create_employee():
 
     try:
         # Check if employee exists
-        if Employee.query.filter_by(emp_email=data["emp_email"].lower().strip()).first():
+        if Employee.query.filter_by(
+            emp_email=data["emp_email"].lower().strip()
+        ).first():
             return jsonify({"error": "Email already exists"}), 400
 
         # Hash password
@@ -36,7 +39,7 @@ def create_employee():
             emp_name=data["emp_name"].strip(),
             emp_email=data["emp_email"].lower().strip(),
             emp_password=hashed_password,
-            created_at=datetime.utcnow()  # Set the creation time
+            created_at=datetime.utcnow(),  # Set the creation time
         )
 
         db.session.add(employee)
@@ -68,14 +71,17 @@ def get_employee(employee_id):
         if not employee:
             return jsonify({"error": "Employee not found"}), 404
 
-        return jsonify(
-            {
-                "id": employee.id,
-                "emp_id": employee.emp_id,
-                "emp_name": employee.emp_name,
-                "emp_email": employee.emp_email,
-            }
-        ), 200
+        return (
+            jsonify(
+                {
+                    "id": employee.id,
+                    "emp_id": employee.emp_id,
+                    "emp_name": employee.emp_name,
+                    "emp_email": employee.emp_email,
+                }
+            ),
+            200,
+        )
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -114,7 +120,11 @@ def update_employee(employee_id):
         if data.get("emp_email"):
             employee.emp_email = data["emp_email"].lower().strip()
         if data.get("emp_password"):
-            employee.emp_password = bcrypt.generate_password_hash(data["emp_password"]).strip().decode("utf-8")
+            employee.emp_password = (
+                bcrypt.generate_password_hash(data["emp_password"])
+                .strip()
+                .decode("utf-8")
+            )
 
         db.session.commit()
 
