73859a86bcac8fe6e898ab50146ff15d7872678a
Anekant  Jain <anekant.jain@Yash.com>
2025-03-28 15:23:09 +0530

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,8 @@ from flask import Flask
 from .config import Config
 from .utils.database import db, bcrypt
 from app.routes.admin_auth import admin_auth
-<<<<<<< HEAD
 from app.routes.meeting_crud import meeting_crud
-=======
 from app.routes.employee_crud import employee_crud
->>>>>>> main
 
 
 def create_app():
@@ -19,11 +16,7 @@ def create_app():
     bcrypt.init_app(app)
 
     app.register_blueprint(admin_auth, url_prefix="/api/admin/auth")
-<<<<<<< HEAD
     app.register_blueprint(meeting_crud, url_prefix="/api/meeting")
-    # app.register_blueprint(employee_crud, url_prefix="/api/employee")
-=======
     app.register_blueprint(employee_crud, url_prefix="/api/employee")
->>>>>>> main
 
     return app
